08838bb609b0a64efba98eeac89a32e84a162929
Mario Bischoff <mario.bischoff@freenet.ag>
2025-10-27 16:09:26 +0100

    !!!

--- src/functions/HelloWorld.ts
+++ src/functions/HelloWorld.ts
@@ -1,7 +1,7 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
 export async function HelloWorld(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+    context.log(`Http function processed request for url "${request.url}!!!"`);
 
     const name = request.query.get('name') || await request.text() || 'world';
 
