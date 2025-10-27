e9918a41b356d774136a6d02bd85b88415a9ccea
Mario Bischoff <mario.bischoff@freenet.ag>
2025-10-27 17:42:06 +0100

    E

--- src/functions/HelloWorld.ts
+++ src/functions/HelloWorld.ts
@@ -5,7 +5,7 @@ export async function HelloWorld(request: HttpRequest, context: InvocationContex
 
     const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello D, ${name}!` };
+    return { body: `Hello E, ${name}!` };
 };
 
 app.http('HelloWorld', {
